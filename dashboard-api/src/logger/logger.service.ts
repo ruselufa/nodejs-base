7ae8873f7b208c95928988c04910ad27c7892f25
Ruslan Galiev <ghaliev@gmail.com>
2023-10-26 12:01:37 +0500

    added controller users2

---
+++ dashboard-api/src/logger/logger.service.ts
@@ -0,0 +1,26 @@
+import {ILogObj, Logger} from 'tslog';
+export class LoggerService {
+    public logger: Logger<ILogObj>;
+
+    constructor() {
+        this.logger = new Logger<any>({
+            type: 'pretty',
+            hideLogPositionForProduction: true
+            // displayInstanceName: false,
+            // displayLoggerName: false,
+            // displayFilePath: 'hidden',
+            // displayFunctionName: false;
+        })
+    }
+    log(...args: unknown[]) {
+        this.logger.info(...args);
+    }
+    error(...args: unknown[]) {
+        // отправка в sentry / rollbar
+        this.logger.error(...args);
+    }
+    warn(...args: unknown[]) {
+        this.logger.warn(...args);
+    }
+
+}
--- st10-studyTs/index.ts
+++ st10-studyTs/index.ts
@@ -1,5 +1,5 @@
 import express, {Request, Response, NextFunction} from "express";
-import { userRouter } from '../src/users/users'
+import { userRouter } from '../dashboard-api/src/users/users'
 
 const port = 8000;
 const app = express();
